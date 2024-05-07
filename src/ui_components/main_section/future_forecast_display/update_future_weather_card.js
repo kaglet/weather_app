895b6843bd42e9705357ee32aede5b3908405a34
Kago <kmotlhabane769@gmail.com>
2024-05-07 19:25:55 +0200

    Update from test data to show future weather in 3 day view

--- src/ui_components/main_section/future_forecast_display/update_future_weather_card.js
+++ src/ui_components/main_section/future_forecast_display/update_future_weather_card.js
@@ -1,13 +1,22 @@
+import storageManager from "../../../in_session_storage/in_session_storage";
 import createMiniInfoCard from "../../info_card/mini_info_card";
+import { format } from "date-fns";
 
 function updateFutureWeatherCard() {
   let futureWeatherCard = document.querySelector(".future.weather.card");
+  let futureWeatherArray = storageManager.getForecastData();
 
-  let miniCard1 = createMiniInfoCard("cold", "34", "37", "Wednesday");
-  let miniCard2 = createMiniInfoCard("cold", "34", "37", "Thursday");
-  let miniCard3 = createMiniInfoCard("cold", "34", "37", "Friday");
+  for (let i = 1; i < 4; i++) {
+    let miniCard = createMiniInfoCard(
+      futureWeatherArray[i].day.condition.text,
+      futureWeatherArray[i].day.mintemp_c,
+      futureWeatherArray[i].day.maxtemp_c,
+      format(new Date(futureWeatherArray[i].date), "cccc"),
+      ""
+    );
 
-  futureWeatherCard.append(miniCard1, miniCard2, miniCard3);
+    futureWeatherCard.append(miniCard);
+  }
 }
 
 export default updateFutureWeatherCard;
